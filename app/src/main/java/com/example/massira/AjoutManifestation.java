22b961fc3ffe6ead8dbd79eee5d821eaa6e9bfd7
kheirou98 <khe-98@hotmail.com>
2019-03-16 19:38:45 +0100

    second commit

--- app/src/main/java/com/example/massira/AjoutManifestation.java
+++ app/src/main/java/com/example/massira/AjoutManifestation.java
@@ -1,13 +1,52 @@
 package com.example.massira;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AjoutManifestation extends AppCompatActivity {
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_manifestation);
+        EditText mDisplayDate = findViewById(R.id.etdate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AjoutManifestation.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                EditText datemanife=findViewById(R.id.etdate);
+                String date = day + "/" + month + "/" + year;
+                datemanife.setText(date);
+            }
+        };
     }
+
 }
